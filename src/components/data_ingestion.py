50a50af718d1ce91bc3a4cffd2f36fb145444850
Melvin Roy V <melvinroy.victor@gmail.com>
2023-04-06 20:48:51 -0400

    Remove artifacts directory from Git repository

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+# 
 @dataclass
 class DataIngestionConfig:
     train_data_path = os.path.join('artifacts',"train.csv")
